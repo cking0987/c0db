325b9cd066e7970098b80295e799edcb86caf715
cking0987 <cking0987@gmail.com>
2017-07-24 00:15:55 -0500

    redid hero and about sections.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-// Sticky nav bar on scroll past hero
+//
+///
+/// Sticky nav bar on scroll past hero
 $(document).ready(function() {
   var heroHeight = $("#heroHeader").outerHeight();
       navBar = $("#navBar");
@@ -11,13 +13,12 @@ $(document).ready(function() {
   });
 });
 
-
+//
+///
+/// Project pages sizing functions
 $(document).ready(function() {
-  // Home Page- Section 1 logo sizing
-    var textHeight = $("#section1Text").outerHeight();
-      $("#section1Logo").css("height", textHeight);
   //Project Pages mockup heights (set on page load)
-  var laptopHeight = $(".laptopMockup").outerHeight();
+    var laptopHeight = $(".laptopMockup").outerHeight();
     $(".iphoneMockup").css("height", laptopHeight);
   //Project Pages mockup heights (adjust on page resize)
   $(window).resize(function(){
@@ -25,3 +26,43 @@ $(document).ready(function() {
         $(".iphoneMockup").css("height", laptopHeight);
   })
 });
+
+//
+///
+///smooth scrolling
+  // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
